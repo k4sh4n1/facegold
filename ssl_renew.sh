cc025c7ab5880dbbb39a7c4bea9cb27dfd642047
k4sh4n1 <kashani@alum.sharif.edu>
2024-12-01 16:44:53 +0330

    Remove `--dry-run` option from certbot renew script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/bin/docker compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/arisa/wordpress/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
