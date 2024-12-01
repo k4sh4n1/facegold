0383d4292e82ce9e008d40698a2ad32e7f64f7b2
k4sh4n1 <kashani@alum.sharif.edu>
2024-12-01 16:27:40 +0330

    `certbot` renew script

---
+++ ssl_renew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMPOSE="/usr/bin/docker compose --no-ansi"
+DOCKER="/usr/bin/docker"
+
+cd /home/arisa/wordpress/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
